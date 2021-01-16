7ef44711ecae601bcc32e1abf2034be850cc371b
NobleActivity <>
2021-01-16 10:31:41 -0800

    lesson 1

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by NobleActivity on 16.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
