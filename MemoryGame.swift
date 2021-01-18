c36d6898d42a5420e9a90ddead740b07ac54aa4c
NobleActivity <>
2021-01-18 12:28:10 -0800

    lesson 3

--- MemoryGame.swift
+++ MemoryGame.swift
@@ -20,8 +20,19 @@ struct MemoryGame<CardContent> {
         cards.shuffle()
     }
     
-    func choose(card: Card) {
+    mutating func choose(card: Card) {
         print("card choosen: \(card)")
+        let chosenIndex: Int = self.index(of: card)
+        cards[chosenIndex].isFaceUp = !cards[chosenIndex].isFaceUp
+    }
+    
+    func index(of card: Card) -> Int {
+        for index in 0..<cards.count {
+            if cards[index].id == card.id {
+                return index
+            }
+        }
+        return 0
     }
     
     struct Card: Identifiable {
